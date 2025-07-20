fd1f40d7f882a4a50b042f6b5a2b5ecb3830ef4f
student-MilanMandal <mandalmilan272@gmail.com>
2025-07-20 11:08:56 +0530

    ADD Navber

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,6 @@ const Navbar = () => {
               src="/public/firstlogo.avif"
               alt=""
               width="95"
-              height="118"
               className="mt-10"
             />
             <div className="flex items-center gap-2">
@@ -18,37 +17,39 @@ const Navbar = () => {
                 src="/public/secondlogo.avif"
                 alt="medswiss logo"
                 width="372"
-                height="99"
               />
             </div>
           </div>
-          <div className="flex mr-20">
-            <img src="/public/thirdlogo.png" alt="" width="222" height="51" />
-            <img src="/public/forthlogo.avif" alt="" width="112" height="40" />
+          <div className="flex mr-20 ">
+            <img src="/public/thirdlogo.png" alt="" width="222" />
+            <img src="/public/forthlogo.avif" alt="" width="112" />
           </div>
         </div>
 
-        <div className="flex flex-wrap gap-2 text-sm font-semibold  mb-[100px] ml-56">
-          <button className=" px-8 py-2  text-green-800 hover:bg-white cursor-pointer border border-black">
+        <div className="flex flex-wrap gap-2 text-sm font-semibold  pb-[600px] ml-[350px] ">
+          <button className=" px-8 py-2  text-green-800 hover:bg-white cursor-pointer border border-black text-[16px]">
             Home
           </button>
-          <button className="bg-red-800 text-white px-10 py-1 hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black">
+          <button className="bg-red-800 text-white px-10 py-1 hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black text-[16px] ">
             About Us
           </button>
-          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black">
+          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black text-[16px]">
             Skill Academy
           </button>
-          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black">
+          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black text-[16px]">
             Certificate Verification
           </button>
-          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black">
+          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black text-[16px]">
             Membership
           </button>
-          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black">
+          <button className="bg-red-800 text-white px-10 py-1  hover:text-green-800 hover:bg-white cursor-pointer hover:border hover:border-black text-[16px]">
             Contact Us
           </button>
         </div>
       </nav>
+      <div className="ml-[120px] bg-slate-100">
+        <img src="/public/logo.avif" alt="" width="1284" />
+      </div>
     </>
   );
 };
